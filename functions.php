b0eb1bbfcb777e386156519bc55dae05f4bb0026
Razaqul Tegar <myrazaqul@gmail.com>
2023-08-05 16:49:01 +0700

    Refactoring

--- functions.php
+++ functions.php
@@ -7,59 +7,34 @@ if (file_exists(get_stylesheet_directory() . '/master/panda-master.php') && !def
 // Checking if Panda SID themes is activated
 function panda_setup()
 {
-    panda_log('Inisialisasi tema Panda SID telah berhasil', 'Sistem Operasi: ' . PHP_OS . ', Versi PHP: ' . PHP_VERSION);
+    add_action('admin_notices', 'panda_notices');
+    panda_logs('Inisialisasi tema Panda SID telah berhasil', 'Sistem Operasi: ' . PHP_OS . ', Versi PHP: ' . PHP_VERSION);
 }
 add_action('after_switch_theme', 'panda_setup');
 
-// Gettings CSS
-function panda_enqueue_styles()
+// Custom Notices
+function panda_notices()
+{
+    echo '
+    <div class="notice notice-info panda-notice is-dismissible">
+        <span class="icon">
+            <img src="' . get_stylesheet_directory_uri() . '/master/assets/images/panda-icons.png" alt="Panda SID" width="250">
+        </span>
+        <div class="notice-content">
+            <h2>Terima kasih telah menggunakan Panda SID, Anda hebat! 🐼</h2>
+            <p>Silahkan lakukan pengaturan awal melalui <a href="admin.php?page=panda_robots">Robot Panda</a>.</p>
+        </div>
+        <button type="button" class="notice-dismiss">
+            <span class="screen-reader-text">Tutup pemberitahuan ini.</span>
+        </button>
+    </div>
+    ';
+}
+
+// Gettings CSS and Scripts
+function panda_enqueue_scripts()
 {
     wp_enqueue_style('parent-style', get_template_directory_uri() . '/style.css');
+    wp_enqueue_script('panda-custom-script', get_stylesheet_directory_uri() . '/scripts.js', array('jquery'), wp_get_theme()->get('Version'), true);
 }
-add_action('wp_enqueue_scripts', 'panda_enqueue_styles');
-
-// Every Divi Layout as Shortcode with the Below Code
-/// New Admin Column
-function pd_create_shortcode_column($columns)
-{
-    $columns['pd_shortcode_id'] = 'Module Shortcode';
-    return $columns;
-}
-add_filter('manage_et_pb_layout_posts_columns', 'pd_create_shortcode_column', 5);
-
-/// Display Shortcode
-function pd_shortcode_content($column, $id)
-{
-    if ('pd_shortcode_id' == $column) {
-?>
-        <p>[sj_layout id="<?php echo $id ?>"]</p>
-<?php
-    }
-}
-add_action('manage_et_pb_layout_posts_custom_column', 'pd_shortcode_content', 5, 2);
-
-/// Create New Shortcode
-function pd_shortcode_mod($pd_mod_id)
-{
-    extract(shortcode_atts(array('id' => '*'), $pd_mod_id));
-    return do_shortcode('[et_pb_section global_module="' . $id . '"][/et_pb_section]');
-}
-add_shortcode('pd_layout', 'pd_shortcode_mod');
-
-function pd_conf_alias()
-{
-    return get_option('conf_alias');
-}
-add_shortcode('conf_alias', 'pd_conf_alias');
-
-function pd_conf_address()
-{
-    return get_option('conf_address');
-}
-add_shortcode('conf_address', 'pd_conf_address');
-
-function pd_conf_phone()
-{
-    return get_option('conf_phone');
-}
-add_shortcode('conf_phone', 'pd_conf_phone');
+add_action('wp_enqueue_scripts', 'panda_enqueue_scripts');
--- master/assets/js/panda_robots.js
+++ master/assets/js/panda_robots.js
@@ -1,5 +1,5 @@
 jQuery(document).ready(function ($) {
-  // jQuery Tabs
+  // Init tabs
   $("#panda-tabs")
     .tabs({
       activate: function (event, ui) {
@@ -11,4 +11,20 @@ jQuery(document).ready(function ($) {
       active: localStorage.getItem("panda-tabs") || 0,
     })
     .show();
+
+  // Helper for swithcing tabs & linking anchors in different tabs
+  $(".toplevel_page_panda_robots").on("click", ".change-tab", function (e) {
+    e.preventDefault();
+
+    $("#panda-tabs").tabs("option", "active", $(this).data("tab"));
+
+    // get the link anchor and scroll to it
+    target = this.href.split("#")[1];
+    if (target) {
+      $.scrollTo("#" + target, 500, { offset: { top: -50, left: 0 } });
+    }
+
+    $(this).blur();
+    return false;
+  });
 });
