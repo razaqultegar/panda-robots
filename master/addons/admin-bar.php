7d29a426bc79a62d24b84735bbfa2ee76d3c8b60
Razaqul Tegar <myrazaqul@gmail.com>
2023-08-10 01:20:55 +0700

    Modified Sitename on Admin Bar

---
+++ master/addons/admin-bar.php
@@ -0,0 +1,39 @@
+<?php
+
+/**
+ * Panda Admin Bar
+ *
+ * This class builds the modify WP Admin bar.
+ *
+ * @package    panda-addons
+ * @author     Puskomedia Indonesia <dev@puskomedia.id>
+ * @copyright  2023 Puskomedia Indonesia
+ * @license    GPL-2.0+
+ **/
+
+if (!defined('ABSPATH')) {
+    exit;
+}
+
+// Modifies the Site name menu
+function panda_site_menu()
+{
+    global $wp_admin_bar;
+
+    $blogname = get_option('conf_alias') . ' ' . get_bloginfo('name');
+
+    if (!$blogname) {
+        $blogname = preg_replace('#^(https?://)?(www.)?#', '', get_home_url());
+    }
+
+    $title = wp_html_excerpt($blogname, 40, '&hellip;');
+
+    $wp_admin_bar->add_node(
+        array(
+            'id'    => 'site-name',
+            'title' => $title,
+            'href'  => home_url('/'),
+        )
+    );
+}
+add_action('wp_before_admin_bar_render', 'panda_site_menu', 999);
